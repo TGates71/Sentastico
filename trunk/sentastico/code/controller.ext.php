a4a735fc2ba099a0ca69f4b882e0c51fafa03b2e
Loetdh <l.dehooge@webimproved.nl>
2016-01-25 11:49:01 +0100

    Made a start for translateble items

--- trunk/sentastico/code/controller.ext.php
+++ trunk/sentastico/code/controller.ext.php
@@ -309,20 +309,20 @@ class module_controller {
 						$random=rand();
 						$sysOS=php_uname('s');
 
-						$line ="<h2>Preparing to install ".$pkgInstall.":</h2>";
+						$line ="<h2>".ui_language::translate ("Preparing to install")." ".$pkgInstall.":</h2>";
 							if (!isset($startinstall)) {
 								if ($pkgdb == "yes") {
-										$line .= "<font color=\"red\"><strong>This package requires a database and database user.</strong></font><br />";
-										$line .= "<a target=\"_blank\" href=\"../../../?module=mysql_databases\">&raquo;Open&laquo; </a> database manager.<br />";
-										$line .= "<a target=\"_blank\" href=\"../../../?module=mysql_users\">&raquo;Open&laquo; </a> database user manager.";
+										$line .= "<font color=\"red\"><strong>".ui_language::translate ("This package requires a database and database user.")."</strong></font><br /><br />";
+										$line .= "<a target=\"_blank\" href=\"../../../?module=mysql_databases\">&raquo;Open&laquo; </a>".ui_language::translate ("database manager.")."<br />";
+										$line .= "<a target=\"_blank\" href=\"../../../?module=mysql_users\">&raquo;Open&laquo; </a> ".ui_language::translate ("database user manager.")."";
 										$line .= "<p>&nbsp;</p>";
 									}
-										$line .= "<p>Please provide the domain and folder name to start the installation of ".$pkgInstall.".</p>";
+										$line .= "<p>".ui_language::translate ("Please provide the domain and folder name to start the installation of")." ".$pkgInstall.".</p>";
 										$line .= "<form id=\"form\" name=\"doInstall\" action=\"/?module=sentastico\" method=\"post\">";
 										$line .= "<table>
 												<tr>
 													<td align=\"right\">
-														<label for=\"site_domain\">Select domain: </label>
+														<label for=\"site_domain\">".ui_language::translate ("Select domain:")." </label>
 													</td>";
 													$line .= "<td align=\"center\">";
 														$list = ListDomain($currentuser['userid']);
@@ -332,25 +332,25 @@ class module_controller {
 												</tr>
 												<tr>
 													<td align=\"right\">
-														<label for=\"install_to_base_dir\">Tick&nbsp;to&nbsp;install to&nbsp;domain&nbsp;root:</label>
+														<label for=\"install_to_base_dir\">".ui_language::translate ("Tick&nbsp;to&nbsp;install to&nbsp;domain&nbsp;root:")."</label>
 													</td>
 													<td align=\"center\">
 														<input type=\"hidden\" name=\"install_to_base_dir\" value=\"0\" />
 														<input type=\"checkbox\" onClick=\"if (this.checked) { document.getElementById('hiderow').style.display = 'none'; } else { document.getElementById('hiderow').style.display = ''; }\" name=\"install_to_base_dir\" value=\"1\" />
 													</td>
 													<td>
-														<font size=\"-1\" color=\"red\"><strong>ALL FILES AND FOLDERS WILL BE DELETED!</strong></font>
+														<font size=\"-1\" color=\"red\"><strong>".ui_language::translate ("ALL FILES AND FOLDERS WILL BE DELETED!")."</strong></font>
 													</td>
 												</tr>
 												<tr id=\"hiderow\">
 													<td align=\"right\">
-														<label for=\"dir_to_install\">Install To Sub-Folder: public_html/[domain]/</label>
+														<label for=\"dir_to_install\">".ui_language::translate ("Install To Sub-Folder:")." public_html/[domain]/</label>
 													</td>
 													<td>
 															<input type=\"text\" name=\"dir_to_install\" style=\"width: 300px\"/>
 													</td>
 													<td>
-															<font color=\"red\">NOTE:</font> For multiple subfolders use: subfolder/subsubfolder
+															<font color=\"red\">".ui_language::translate ("NOTE:")."</font>".ui_language::translate (" For multiple subfolders use: subfolder/subsubfolder")."
 													</td>
 												</tr>
 												<tr>

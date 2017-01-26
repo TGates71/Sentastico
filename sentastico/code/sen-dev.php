fe9b80196fd8fb8c17212d62b6495462f1a639a6
TGates <tgates@mach-hosting.com>
2017-01-26 10:23:41 -0500

    Fixes

--- sentastico/code/sen-dev.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Sentastico Open Source Developer File for Sentora CP
-// Updated By       : TGates for Sentora
-// Additional Work  : Durandle, Mudasir Mirza
-// Contact Email    : tgates@sentora.org
-// Original Author  : Bobby Allen
-?>
